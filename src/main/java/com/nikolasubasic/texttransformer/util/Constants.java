8a1e53220a8464f5e85d7cf271ac7f17bf55301b
Nikola Subasic <nikola.subasic@qcerris.com>
2024-06-25 18:19:18 +0200

    added some tests. Added exception handilng

--- src/main/java/com/nikolasubasic/texttransformer/util/Constants.java
+++ src/main/java/com/nikolasubasic/texttransformer/util/Constants.java
@@ -3,4 +3,7 @@ package com.nikolasubasic.texttransformer.util;
 public class Constants {
     public static final String PARAM_REGEX = "regex";
     public static final String PARAM_REPLACEMENT = "replacement";
+    public static final String TRANSLITERATE_TRANSFORMER = "transliterate";
+    public static final String REMOVE_REGEX_TRANSFORMER = "removeRegex";
+    public static final String REPLACE_REGEX_TRANSFORMER = "replaceRegex";
 }
---
+++ src/test/java/com/nikolasubasic/texttransformer/service/TransformerServiceTest.java
@@ -0,0 +1,101 @@
+package com.nikolasubasic.texttransformer.service;
+
+import com.nikolasubasic.texttransformer.model.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static com.nikolasubasic.texttransformer.util.Constants.*;
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.*;
+
+public class TransformerServiceTest {
+    private static final String TEST_GROUP_ID = "test_group_id";
+    private static final String TEST_REGEX_LOWER = "test";
+    private static final String TEST_REGEX_UPPER = "TEST";
+    private static final String TEST_VALUE = "TESTtest";
+    private static final String TEST_VALUE_AFTER_REPLACEMENT = "replacementForTest_ИнДифферентАлпхабет";
+    private static final String TEST_VALUE_TRANSLITERATED = "replacementForTest_InDifferentAlphabet";
+    private static final String TEST_VALUE_NOT_FOR_TRANSFORMATION = "value not to be transformed";
+
+    @Mock
+    private TransformerFactory transformerFactory;
+
+    @InjectMocks
+    private TransformerServiceImpl transformerService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void testTransformElementsWithMultipleTransformers() {
+        Element element = createElementWithAllTransformers();
+
+        when(transformerFactory.getTransformer("removeRegex")).thenReturn((value, params) -> TEST_REGEX_LOWER);
+        when(transformerFactory.getTransformer("replaceRegex")).thenReturn((value, params) -> TEST_VALUE_AFTER_REPLACEMENT);
+        when(transformerFactory.getTransformer("transliterate")).thenReturn((value, params) -> TEST_VALUE_TRANSLITERATED);
+
+        List<TransformationResponse> results = transformerService.transformElements(Collections.singletonList(element));
+        assertEquals(1, results.size());
+        assertEquals(TEST_VALUE_TRANSLITERATED, results.get(0).getTransformedValue());
+    }
+
+    @Test
+    void testTransformElementsWithEmptyTransformers() {
+        Element element = new Element();
+        element.setTransformers(Collections.emptyList());
+        element.setValue(TEST_VALUE_NOT_FOR_TRANSFORMATION);
+
+        List<TransformationResponse> results = transformerService.transformElements(Collections.singletonList(element));
+        assertEquals(1, results.size());
+        assertEquals(TEST_VALUE_NOT_FOR_TRANSFORMATION, results.get(0).getTransformedValue());
+    }
+
+    @Test
+    void testTransformElementsWithNullAndEmptyCollection() {
+        List<TransformationResponse> results = transformerService.transformElements(null);
+        assertTrue(results.isEmpty());
+
+        results = transformerService.transformElements(Collections.emptyList());
+        assertTrue(results.isEmpty());
+    }
+
+    private Element createElementWithAllTransformers() {
+        Transformer removeRegexTransformer = getTransformer(REMOVE_REGEX_TRANSFORMER, TEST_REGEX_UPPER);
+        Transformer replaceRegexTransformer = getTransformer(REPLACE_REGEX_TRANSFORMER, TEST_REGEX_LOWER);
+        Transformer transliterateTransformer = getTransformer(TRANSLITERATE_TRANSFORMER, "");
+
+        Element element = new Element();
+        element.setValue(TEST_VALUE);
+        element.setTransformers(Arrays.asList(replaceRegexTransformer, removeRegexTransformer, transliterateTransformer));
+
+        return element;
+    }
+
+    private Transformer getTransformer(String type, String regex) {
+        Transformer transformer = new Transformer();
+        Map<String, String> parameters = new HashMap<>();
+
+        switch (type) {
+            case REPLACE_REGEX_TRANSFORMER: {
+                String replacement = TEST_VALUE_AFTER_REPLACEMENT;
+                parameters.put(PARAM_REPLACEMENT, replacement);
+            }
+            case REMOVE_REGEX_TRANSFORMER: {
+                parameters.put(PARAM_REGEX, regex);
+            }
+            case TRANSLITERATE_TRANSFORMER: {
+                transformer.setTransformerId(type);
+            }
+        }
+        transformer.setParameters(parameters);
+        transformer.setGroupId(TEST_GROUP_ID);
+        return transformer;
+    }
+}
